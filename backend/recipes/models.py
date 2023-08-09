45ad5495fcf716b1852cc30953dc3a83787002a0
Daniil Fedotov <pustovdanil@bk.ru>
2023-08-09 19:08:58 +0500

    del user model

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,35 +1,14 @@
 from django.db import models
-#from django.contrib.auth.models import AbstractUser
 
-from users.models import User
-
-"""
-class User(AbstractUser):
-    email = models.EmailField(
-        max_length=256,
-        unique=True,)
-    username = models.CharField(
-        max_length=150,
-        unique=True)
-    first_name = models.CharField(
-        max_length=150,
-        unique=True)
-    last_name = models.CharField(
-        max_length=150,
-        unique=True)
-
-    def __str__(self):
-        return f'{self.username}'
-"""
 
 class Tag(models.Model):
     name = models.CharField(
         max_length=200,
-        null=True,
+        unique=True,
         verbose_name='Название тега')
     color = models.CharField(
         max_length=7,
-        null=True,
+        unique=True,
         verbose_name='Цвет тега')
     slug = models.SlugField(
         max_length=200,
@@ -37,19 +16,20 @@ class Tag(models.Model):
         verbose_name='Раздел тега')
 
     def __str__(self):
-        return f'{self.name}'
+        return self.name
 
 
 class Recipe(models.Model):
     tags = models.ForeignKey(
         Tag,
-        null=True,
         on_delete=models.SET_NULL,
         related_name='recipes')
+    """
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='recipes')
+    """
     ingredients = models.ManyToManyField(
         'Ingredient',
         through='RecipeIngredient')
@@ -66,9 +46,8 @@ class Recipe(models.Model):
     cooking_time = models.PositiveIntegerField(
         verbose_name='Время приготовления (в минутах)')
 
-
     def __str__(self):
-        return f'{self.name}'
+        return self.name
 
 
 class Ingredient(models.Model):
@@ -79,7 +58,7 @@ class Ingredient(models.Model):
         max_length=200)
 
     def __str__(self):
-        return f'{self.name}'
+        return self.name
 
 
 class RecipeIngredient(models.Model):
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,6 @@
+"""
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-#from .user_roles import UserRoles
 
 
 class User(AbstractUser):
@@ -17,26 +16,4 @@ class User(AbstractUser):
     last_name = models.CharField(
         max_length=150,
         unique=True)
-    """
-    role = models.CharField(
-    max_length=150,
-    verbose_name='Роль',
-    choices=UserRoles.choices,
-    default=UserRoles.NOT_AUTH_USER,)
-    """
-
-    def __str__(self):
-        return f'{self.username}'
-"""
-    @property
-    def is_admin(self):
-        return self.role == UserRoles.ADMIN
-
-    @property
-    def is_auth_user(self):
-        return self.role == UserRoles.AUTH_USER
-
-    @property
-    def is_not_auth_user(self):
-        return self.role == UserRoles.NOT_AUTH_USER
 """
