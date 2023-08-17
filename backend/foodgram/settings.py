dc741b6e3bee371090e51defe79b322d59727219
Daniil Fedotov <pustovdanil@bk.ru>
2023-08-17 23:55:39 +0500

    set docker

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-l2#856c+o8k+h!ddpzuy^j3yx#$j*%*x6t5zhl+zzf1me28452
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
